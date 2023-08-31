4e67e8e7ff337298f78c38607aa648e13d33c0e7
Ndarugaa <ndaruga.francis@gmail.com>
2023-08-31 09:18:03 +0300

    add user to sudoers

--- conf-ansible.sh
+++ conf-ansible.sh
@@ -26,11 +26,7 @@ validate_ip(){
 
 consec=0
 
-# Loop until the Esc key is pressed
-#if [[ $(id -u) != 0 ]]; then
-#	echo "You must be root to run this script"
-#	sudo su
-#fi
+# Loop until 
 
 while true; do
 	read -p "Please enter an IP address (press Enter twice to exit): " ip_add
@@ -61,4 +57,22 @@ echo
 read -p "New User: " username
 sudo adduser $username
 sudo passwd $username
+
+# Add User to the sudo Group
+echo "Enter root Password then type 'exit'"
+su root
+
+# Check if the user is already in the sudoers file
+if grep -q "$username ALL=(ALL) NOPASSWD: ALL" "/etc/sudoers"; then
+    echo "User $username already in sudoers file"
+else
+    echo "$username ALL=(ALL) NOPASSWD: ALL" >> "/etc/sudoers"
+	echo "Adding user $username to sudoers file"
+fi
+su root
+sudo chmod 440 /etc/sudoers
+
+# Restart the ssh server
+sudo systemctl restart sshd
+
 exit
