18710c7cc25bf0170c6e04164ff8563f03b160e8
Ndarugaa <ndaruga.francis@gmail.com>
2023-09-08 18:06:50 +0300

    Modify ram

--- Manage-VMs/modifyVM.bat
+++ Manage-VMs/modifyVM.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d "C:\Program Files\Oracle\VirtualBox"
+setlocal enabledelayedexpansion
 
-setlocal
 set /p vmName=Enter VM name to modify: 
-echo WARNING: Modifying a Virtual Machine require that the machine is powered off, neither running nor in a Saved state
-echo Power Off the VM %vmName% (Y/n)
+echo WARNING: Modifying a Virtual Machine requires that the machine is powered off, neither running nor in a Saved state.
+echo Power Off the VM %vmName% (Y/n)?
 set /p choice=
 if "%choice%" == "y" (
     VBoxManage controlvm %vmName% poweroff
@@ -12,41 +12,69 @@ if "%choice%" == "y" (
     goto end
 )
 
-:end
+:options
+cls
 echo.
 echo GENERAL VIRTUAL MACHINE SETTINGS
 echo 1: Allocate amount of RAM
 echo 2: Setting up a virtual hard drive
-echo 2: Setting up and attaching the Installation Media (e.g., ISO)
-
+echo 3: Setting up and attaching the Installation Media (e.g., ISO)
 echo.
-
 set /p option=Enter an option:
 echo.
 
-:: Check the option and run the corresponding command
-if "%option%"=="1" (
-    set /p ram=Enter amount of RAM in MBs: 
-    VBoxManage modifyvm %vmName% --memory %ram%
-    
-) else if "%option%"=="2" (
-    echo Registered VMs
-    VBoxManage list -s vms
-) else if "%option%"=="3" (
-    echo Running VMs
-    VBoxManage list runningvms
-) else if "%option%"=="4" (
-    VBoxManage list ostypes
-) else if "%option%"=="5" (
-    call %~dp0createvm.bat
-) else if "%option%"=="6" (
-    call %~dp0modifyVM.bat
-) else if "%option%"=="7" (
-    call %~dp0startvm.bat
-) else if "%option%"=="8" (
-    call %~dp0controlVM.bat
+if "%option%"=="1" goto RamMenu
+if "%option%"=="2" goto HardDriveMenu
+if "%option%"=="3" echo "Installation Media option not yet implemented" & pause & goto options
+
+echo You entered an Invalid option.
+pause
+goto options
+
+:RamMenu
+cls
+echo.
+echo Menu:
+echo 1. Set VM RAM to 512MB
+echo 2. Set VM RAM to 1024MB
+echo 3. Set VM RAM to 2048MB
+echo 9. Custom RAM Size
+echo 0. Exit to Main Menu
+echo.
+set /p ram=Select RAM option: 
+
+if "%ram%"=="0" goto options
+call :setRamSize %ram% %vmName%
+goto RamMenu
+
+:setRamSize
+if "%1"=="1" (
+    set ram_size=512
+) else if "%1"=="2" (
+    set ram_size=1024
+) else if "%1"=="3" (
+    set ram_size=2048
+) else if "%1"=="9" (
+    set /p ram_size=Enter custom RAM size in MB: 
 ) else (
-    echo You entered an Invalid option
+    echo Invalid RAM option.
+    pause
+    goto RamMenu
 )
 
-pause
+VBoxManage modifyvm %2 --memory %ram_size%
+if errorlevel 1 (
+    echo Error modifying memory of %2.
+    pause
+    goto RamMenu
+) else (
+    echo Memory of %2 modified to %ram_size%MB successfully!
+    pause
+    goto RamMenu
+)
+
+:HardDriveMenu
+
+
+:end
+exit /b
